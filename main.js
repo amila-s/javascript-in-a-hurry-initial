b2200a0342d1acd501f4d36d4c4cddede8615b58
Amila <amilathegreat@gmail.com>
2024-12-15 18:50:01 -0500

    Continued course, about 1/4 through 'advanced javascript' module

--- main.js
+++ main.js
@@ -1,54 +1,3 @@
-
-// Menu Section
-document.querySelector("#open-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.add("nav-open");
-});
-
-document.querySelector("#close-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
-});
-
-// Greeting Section
-
-function celsiusToFahr(temperature){
-    let fahr = (temperature * 9/5) + 32;
-    return fahr;
-}
-
-
-const greetingText = "Good afternoon!";
-const weatherCondition = "sunny";
-const userLocation = "Mumbai";
-let temperature = 30;
-
-alert("The temperature outside is " + celsiusToFahr(temperature) + "°F")
-
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
-let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
-
-document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = celsiusText;
-
-document.querySelector(".weather-group").addEventListener("click", function(e){
-
-    //can also use e["target"]
-    if (e.target.id == "celsius") {
-        document.querySelector("p#weather").innerHTML = celsiusText;
-    } else if (e.target.id == "fahr") {
-        document.querySelector("p#weather").innerHTML = fahrText;
-    } 
-});
-
-
-setInterval(function(){
-    let localTime = new Date();
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
-},1000);
-
-//Gallery Section
-
 const galleryImages= [
     {
         src: "./assets/gallery/image1.jpg",
@@ -63,28 +12,106 @@ const galleryImages= [
         alt: "Thumbnail Image 3"
     }
 ];
+// Menu Section
+function menuHandler(){
+    document.querySelector("#open-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.add("nav-open");
+    });
+    
+    document.querySelector("#close-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
+}
 
-// for(let i in galleryImages) {
-//     console.log(galleryImages[i]);
-// }
+// Greeting Section
+function greetingHandler() {
+    let currentHour = new Date().getHours();
+    let greetingText;
 
-let mainImage = document.querySelector("#gallery > img");
-let thumbnails = document.querySelector("#gallery .thumbnails");
+    if (currentHour < 12) {
+        greetingText = "Good morning!";
+    } else if (currentHour < 19) {
+        greetingText = "Good afternoon!";
+    } else if (currentHour < 24) {
+        greetingText = "Good evening!";
+    } else {
+        greetingText = "Welcome!";
+    }
 
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
+    const weatherCondition = "sunny";
+    const userLocation = "Mumbai";
+    let temperature = 30;
 
-// img src="./assets/gallery/image1.jpg" 
-// alt="Thumbnail Image 1" 
-// data-array-index="0" 
-// data-selected="true"
+    let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
+    let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
 
-galleryImages.forEach(function(image, index){
-    let thumb = document.createElement("img");
-    thumb.src=image.src;
-    thumb.alt=image.alt;
-    thumb.dataset.arrayIndex = index;
-    thumb.dataset.selected = false;
-    thumbnails.appendChild(thumb);
-});
+    document.querySelector("#greeting").innerHTML = greetingText;
+    document.querySelector("p#weather").innerHTML = celsiusText;
 
+    document.querySelector(".weather-group").addEventListener("click", function(e){
+
+        //can also use e["target"]
+        if (e.target.id == "celsius") {
+            document.querySelector("p#weather").innerHTML = celsiusText;
+        } else if (e.target.id == "fahr") {
+            document.querySelector("p#weather").innerHTML = fahrText;
+        } 
+    });
+}
+
+// Temperature Conversion 
+function celsiusToFahr(temperature){
+    let fahr = (temperature * 9/5) + 32;
+    return fahr;
+}
+
+// Local Time Section
+function clockHandler() {
+    setInterval(function(){
+        let localTime = new Date();
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2,"0");
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2,"0");
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2,"0");
+    },1000);
+}
+
+//Gallery Section
+function galleryHandler() {
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
+
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
+
+
+    galleryImages.forEach(function(image, index){
+        let thumb = document.createElement("img");
+        thumb.src=image.src;
+        thumb.alt=image.alt;
+        thumb.dataset.arrayIndex = index;
+        //ternary operator
+        //condition ? expressionIfTrue : expressionIfFalse;
+        thumb.dataset.selected = index === 0 ? true : false;
+
+        thumb.addEventListener("click", function(e){
+            let selectedIndex = e.target.dataset.arrayIndex;
+            let selectedImage = galleryImages[selectedIndex];
+            mainImage.src = selectedImage.src;
+            mainImage.alt = selectedImage.alt;
+
+            thumbnails.querySelectorAll("img").forEach(function(img){
+                img.dataset.selected = false;
+            });
+            e.target.dataset.selected = true;
+        })
+
+        thumbnails.appendChild(thumb);
+    });
+}
+
+
+//Page Load
+menuHandler();
+greetingHandler();
+clockHandler();
+galleryHandler();
