f44e69c4ed09703f0af100e62da8d0da40d51cc4
Amila <amilathegreat@gmail.com>
2024-12-29 07:39:26 -0500

    Finish javascript in a hurry training course

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const weatherAPIKey = "8dc06072ed98cdadb99b373108b96602"
+const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric`
+
 const galleryImages= [
     {
         src: "./assets/gallery/image1.jpg",
@@ -38,24 +41,46 @@ function greetingHandler() {
         greetingText = "Welcome!";
     }
 
-    const weatherCondition = "sunny";
-    const userLocation = "Mumbai";
-    let temperature = 30;
-
-    let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`;
-    let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
-
     document.querySelector("#greeting").innerHTML = greetingText;
-    document.querySelector("p#weather").innerHTML = celsiusText;
 
-    document.querySelector(".weather-group").addEventListener("click", function(e){
 
-        //can also use e["target"]
-        if (e.target.id == "celsius") {
+}
+
+//Weather Text
+function weatherHandler(){
+    navigator.geolocation.getCurrentPosition(position => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        let url = weatherAPIURL
+            .replace("{lat}",latitude)
+            .replace("{lon}",longitude)
+            .replace("{API key}",weatherAPIKey);
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const condition = data.weather[0].description;
+            const location = data.name;
+            const temperature = data.main.temp;
+
+            let celsiusText = `The weather is ${condition} in ${location} and it's ${temperature.toFixed(1)}°C outside.`;
+            let fahrText = `The weather is ${condition} in ${location} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside.`;
+        
+            
             document.querySelector("p#weather").innerHTML = celsiusText;
-        } else if (e.target.id == "fahr") {
-            document.querySelector("p#weather").innerHTML = fahrText;
-        } 
+        
+            document.querySelector(".weather-group").addEventListener("click", function(e){
+        
+                //can also use e["target"]
+                if (e.target.id == "celsius") {
+                    document.querySelector("p#weather").innerHTML = celsiusText;
+                } else if (e.target.id == "fahr") {
+                    document.querySelector("p#weather").innerHTML = fahrText;
+                } 
+            });
+        }).catch((err => {
+            document.querySelector("p#weather").innerHTML = "Unable to get the weather info. Try again later."
+        }));
+    
     });
 }
 
@@ -109,9 +134,140 @@ function galleryHandler() {
     });
 }
 
+//Products Section
+
+const products = [
+        {
+          title: "AstroFiction",
+          author: "John Doe",
+          price: 49.9,
+          image: "./assets/products/img6.png"
+        },
+        {
+          title: "Space Odissey",
+          author: "Marie Anne",
+          price: 35,
+          image: "./assets/products/img1.png"
+        },
+        {
+          title: "Doomed City",
+          author: "Jason Cobert",
+          price: 0,
+          image: "./assets/products/img2.png"
+        },
+        {
+          title: "Black Dog",
+          author: "John Doe",
+          price: 85.35,
+          image: "./assets/products/img3.png"
+        },
+        {
+          title: "My Little Robot",
+          author: "Pedro Paulo",
+          price: 0,
+          image: "./assets/products/img5.png"
+        },
+        {
+          title: "Garden Girl",
+          author: "Ankit Patel",
+          price: 45,
+          image: "./assets/products/img4.png"
+        }
+]
+
+function populateProducts(productList) {
+
+    let productsSection = document.querySelector(".products-area");
+    productsSection.textContent = "";
+
+    productList.forEach(function(product, index){
+        // Create the HTML element for the individual product
+        let productElm = document.createElement("div");
+        productElm.classList.add("product-item");
+
+        // Create the product image
+        let productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = "Image for " + product.title;
+
+        // Create the product details section
+        let productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        // Create the the product title, author, pricetitle and price
+        let productTitle = document.createElement("h3");
+        productTitle.classList.add("product-title");
+        productTitle.textContent = product.title;
+
+        let productAuthor = document.createElement("p");
+        productAuthor.classList.add("product-author");
+        productAuthor.textContent = product.author;
+
+        let priceTitle = document.createElement("p");
+        priceTitle.classList.add("price-title");
+        priceTitle.textContent = "Price";
+
+        let productPrice = document.createElement("p");
+        productPrice.classList.add("product-price");
+        productPrice.textContent = product.price > 0 ? "$" + product.price.toFixed(2) : "Free";
+
+
+        // Append the product details
+        productDetails.append(productTitle);
+        productDetails.append(productAuthor);
+        productDetails.append(priceTitle);
+        productDetails.append(productPrice);
+
+        // Add all child HTML elements for the product
+        productElm.append(productImage);
+        productElm.append(productDetails);
+
+        // Add complete individual product to the product section
+        productsSection.append(productElm);
+    });    
+}
+
+function productsHandler() {
+
+    let freeProducts = products.filter(item => !item.price || item.price <= 0);
+
+    let paidProducts = products.filter(item => item.price > 0);
+
+    populateProducts(products);
+
+    document.querySelector(".products-filter label[for=all] span.product-amount").textContent = products.length;
+    document.querySelector(".products-filter label[for=paid] span.product-amount").textContent = paidProducts.length;
+    document.querySelector(".products-filter label[for=free] span.product-amount").textContent = freeProducts.length;
+
+    let productsFilter = document.querySelector(".products-filter");
+
+    productsFilter.addEventListener("click", function(e){
+        if (e.target.id === "all") {
+            populateProducts(products);
+        } else if (e.target.id === "paid") {
+            populateProducts(paidProducts);
+        } else if (e.target.id === "free") {
+            populateProducts(freeProducts);
+        }
+    });
+
+}
+
+function footerHandler(){
+    let currentYear = new Date().getFullYear();
+    document.querySelector("footer").textContent = `© ${currentYear} - All rights reserved`;
+}
+
+
+
+//8dc06072ed98cdadb99b373108b96602
+
 
 //Page Load
 menuHandler();
 greetingHandler();
 clockHandler();
 galleryHandler();
+productsHandler();
+footerHandler();
+weatherHandler();
